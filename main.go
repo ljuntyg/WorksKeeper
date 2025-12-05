114489880a6f33ed83e0b2fb9757bf07a9d7dfe6
ljuntyg <gushedlunda@gmail.com>
2025-12-05 21:12:00 +0100

    Add simple canvas for creating works with content

--- main.go
+++ main.go
@@ -2,51 +2,79 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"html/template"
+	"io"
+	"iter"
 	"log"
-	v2 "math/rand/v2"
+	"maps"
+	"math/rand/v2"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"sort"
+	"strconv"
+
+	"github.com/google/uuid"
 )
 
 type Listing interface {
 	GetTitle() string
 	GetTimeRequiredMinutes() int
 	GetUrl() string
+	GetId() uuid.UUID
 }
 
 type Series struct {
 	Title string
 	Works []Listing
+	Id    uuid.UUID
 }
 
 type Work struct {
 	Title    string
 	Length   int
 	Contents []Content
+	Id       uuid.UUID
 }
 
 // TODO: add GetCaption() method
 type Content interface {
-	GetContent() []string
+	GetSourceUrls() []string
 	GetType() ContentType
 	ToHTML() template.HTML
+	GetIndex() int
+	GetCaption() string
+	GetId() uuid.UUID
+	SetIndex(idx int)
 }
 
 type Text struct {
-	Text string
+	Text  string
+	Index int
+	Id    uuid.UUID
 }
 
 type Sound struct {
 	SoundPaths []string
+	Index      int
+	Caption    string
+	Id         uuid.UUID
 }
 
 type Video struct {
 	VideoPaths []string
+	Index      int
+	Caption    string
+	Id         uuid.UUID
 }
 
 type Image struct {
 	ImagePaths []string
+	Index      int
+	Caption    string
+	Id         uuid.UUID
 }
 
 type ContentType int
@@ -65,23 +93,84 @@ var AllContentTypes = []ContentType{
 	ImageType,
 }
 
-const textTemplate = `<p>{{index .GetContent 0}}</p>`
+var MimeToContentType = map[string]ContentType{
+	"image/apng":      ImageType,
+	"image/avif":      ImageType,
+	"image/bmp":       ImageType,
+	"image/gif":       ImageType,
+	"image/jpeg":      ImageType,
+	"image/png":       ImageType,
+	"image/svg+xml":   ImageType,
+	"image/tiff":      ImageType,
+	"image/webp":      ImageType,
+	"audio/aac":       SoundType,
+	"audio/midi":      SoundType,
+	"audio/x-midi":    SoundType,
+	"audio/mpeg":      SoundType,
+	"audio/ogg":       SoundType,
+	"audio/wav":       SoundType,
+	"audio/webm":      SoundType,
+	"audio/3gpp":      SoundType,
+	"audio/3gpp2":     SoundType,
+	"application/ogg": SoundType, // TODO: ? some .ogg files
+	"video/mp4":       VideoType,
+	"video/mpeg":      VideoType,
+	"video/ogg":       VideoType,
+	"video/webm":      VideoType,
+	"video/x-msvideo": VideoType,
+	"video/mp2t":      VideoType,
+	"video/3gpp":      VideoType,
+	"video/3gpp2":     VideoType,
+}
 
-const soundTemplate = `<audio controls>
-	{{range .GetContent}}
+const textTemplate = `<p>{{index .GetSourceUrls 0}}</p>`
+
+/* const soundTemplate = `<audio controls>
+	{{range .GetSourceUrls}}
 	<source src={{.}}>
 	{{end}}
 	Your browser doesn't support this audio.
-</audio>`
+</audio>` */
 
-const videoTemplate = `<video controls>
-	{{range .GetContent}}
+const soundTemplate = `<figure>
+	<audio controls>
+		{{range .GetSourceUrls}}
+		<source src={{.}}>
+		{{end}}
+		Your browser doesn't support this audio.
+	</audio>
+	<figcaption>{{.GetCaption}}</figcaption>
+</figure>`
+
+/* const videoTemplate = `<video controls>
+	{{range .GetSourceUrls}}
 	<source src={{.}}>
 	{{end}}
 	Your browser doesn't support this video.
-</video>`
+</video>` */
 
-const imageTemplate = `<img src={{index .GetContent 0}} alt="Image">`
+const videoTemplate = `<figure>
+	<video controls>
+		{{range .GetSourceUrls}}
+		<source src={{.}}>
+		{{end}}
+		Your browser doesn't support this video.
+	</video>
+	<figcaption>{{.GetCaption}}</figcaption>
+</figure>`
+
+/* const imageTemplate = `<img src={{index .GetSourceUrls 0}} alt="Image">` */
+
+const imageTemplate = `<figure>
+	<img src={{index .GetSourceUrls 0}} alt="Image">
+	<figcaption>{{.GetCaption}}</figcaption>
+</figure>`
+
+// ----------------------------
+//
+//	LISTINGS LISTINGS LISTINGS
+//
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
 func (s *Series) GetTitle() string {
 	return s.Title
@@ -100,7 +189,11 @@ func (s *Series) GetTimeRequiredMinutes() int {
 func (s *Series) GetUrl() string {
 	log.Println("calling MOCK GetUrl()")
 
-	return getBaseUrl() + "/series/with/id/MOCK"
+	return getBaseUrl() + "/series/number/" + s.Id.String()
+}
+
+func (s *Series) GetId() uuid.UUID {
+	return s.Id
 }
 
 func (w *Work) GetTitle() string {
@@ -119,10 +212,32 @@ func (w *Work) GetUrl() string {
 	log.Println("calling MOCK GetUrl()")
 
 	// TODO: the returned URL should be a sudbomain, the proxy will reverse it
-	return getBaseUrl() + "/works/with/id/MOCK"
+	return getBaseUrl() + "/work/number/" + w.Id.String()
 }
 
-func (t *Text) GetContent() []string {
+func (w *Work) GetId() uuid.UUID {
+	return w.Id
+}
+
+// TODO:
+func (w *Work) addContent(c Content) {
+	w.Contents = append(w.Contents, c)
+	c.SetIndex(len(w.Contents) - 1)
+
+	sort.Slice(w.Contents, func(i, j int) bool {
+		return w.Contents[i].GetIndex() < w.Contents[j].GetIndex()
+	})
+
+	GetExistingMockDb().Contents[c.GetId()] = c
+}
+
+// ----------------------------
+//
+//	CONTENTS CONTENTS CONTENTS
+//
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+func (t *Text) GetSourceUrls() []string {
 	return []string{t.Text}
 }
 
@@ -141,7 +256,23 @@ func (t *Text) ToHTML() template.HTML {
 	return template.HTML(buf.String())
 }
 
-func (s *Sound) GetContent() []string {
+func (t *Text) GetIndex() int {
+	return t.Index
+}
+
+func (t *Text) GetCaption() string {
+	return ""
+}
+
+func (t *Text) GetId() uuid.UUID {
+	return t.Id
+}
+
+func (t *Text) SetIndex(idx int) {
+	t.Index = idx
+}
+
+func (s *Sound) GetSourceUrls() []string {
 	return s.SoundPaths
 }
 
@@ -160,7 +291,23 @@ func (s *Sound) ToHTML() template.HTML {
 	return template.HTML(buf.String())
 }
 
-func (v *Video) GetContent() []string {
+func (s *Sound) GetIndex() int {
+	return s.Index
+}
+
+func (s *Sound) GetId() uuid.UUID {
+	return s.Id
+}
+
+func (s *Sound) GetCaption() string {
+	return s.Caption
+}
+
+func (s *Sound) SetIndex(idx int) {
+	s.Index = idx
+}
+
+func (v *Video) GetSourceUrls() []string {
 	return v.VideoPaths
 }
 
@@ -179,7 +326,23 @@ func (v *Video) ToHTML() template.HTML {
 	return template.HTML(buf.String())
 }
 
-func (i *Image) GetContent() []string {
+func (v *Video) GetIndex() int {
+	return v.Index
+}
+
+func (v *Video) GetCaption() string {
+	return v.Caption
+}
+
+func (v *Video) GetId() uuid.UUID {
+	return v.Id
+}
+
+func (v *Video) SetIndex(idx int) {
+	v.Index = idx
+}
+
+func (i *Image) GetSourceUrls() []string {
 	return i.ImagePaths
 }
 
@@ -198,6 +361,28 @@ func (i *Image) ToHTML() template.HTML {
 	return template.HTML(buf.String())
 }
 
+func (i *Image) GetIndex() int {
+	return i.Index
+}
+
+func (i *Image) GetCaption() string {
+	return i.Caption
+}
+
+func (i *Image) GetId() uuid.UUID {
+	return i.Id
+}
+
+func (i *Image) SetIndex(idx int) {
+	i.Index = idx
+}
+
+// --------------------------------------
+//
+//	CONTENTTYPE CONTENTTYPE CONTENTTYPE
+//
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
 func (ct ContentType) String() string {
 	switch ct {
 	case TextType:
@@ -213,26 +398,73 @@ func (ct ContentType) String() string {
 	}
 }
 
+func (ct ContentType) SingularString() string {
+	str := ct.String()
+	if str[len(str)-1:] == "s" {
+		return str[:len(str)-1]
+	} else {
+		return str
+	}
+}
+
+// This should NOT be used for content source links
 func (ct ContentType) mediaDir() string {
 	if ct.String() == "unknown" {
 		panic("Unexpected ContentType when reading mediaDir()")
 	}
 
-	return "./resources/media/" + ct.String()
+	return "/resources/media/" + ct.String() + "/"
 }
 
+// This should be used for content source links
 func (ct ContentType) urlPrefix() string {
 	if ct.String() == "unknown" {
 		panic("Unexpected ContentType when reading urlPrefix()")
 	}
 
-	return "/" + ct.String() + "/with/name/"
+	return "/" + ct.SingularString() + "/with/name/"
 }
 
-// TODO:
-func getNWorkListings(n int) []Listing {
-	return GetNMockListings(n)
+func (ct ContentType) toLocalPath(fileName string) string {
+	return "." + ct.mediaDir() + fileName
 }
+
+func (ct ContentType) toSourceUrl(fileName string) string {
+	return getBaseUrl() + ct.urlPrefix() + fileName
+}
+
+func (ct ContentType) createNew(sources []string) Content {
+	switch ct {
+	case TextType:
+		return &Text{
+			Text: sources[0],
+			Id:   uuid.New(),
+		}
+	case SoundType:
+		return &Sound{
+			SoundPaths: sources,
+			Id:         uuid.New(),
+		}
+	case ImageType:
+		return &Image{
+			ImagePaths: sources,
+			Id:         uuid.New(),
+		}
+	case VideoType:
+		return &Video{
+			VideoPaths: sources,
+			Id:         uuid.New(),
+		}
+	default:
+		panic("unknown ContentType when creating empty")
+	}
+}
+
+// --------------------------
+//
+//	HELPERS HELPERS HELPERS
+//
+// \/\/\/\/\/\/\/\/\/\/\/\/\/
 
 func formatTimeRequired(min int) string {
 	if min >= 60 {
@@ -245,33 +477,187 @@ func formatTimeRequired(min int) string {
 }
 
 // TODO:
+func addListing(l Listing) {
+	GetExistingMockDb().Listings[l.GetId()] = l
+}
+
+// TODO:
 func getBaseUrl() string {
 	return GetMockBaseUrl()
 }
 
 // TODO:
-func getWork(id string) *Work {
-	return GetMockWork(99)
+func createNewWork() *Work {
+	log.Println("calling MOCK createNewWork()")
+
+	work := &Work{
+		Title:  strconv.Itoa(rand.IntN(100)),
+		Length: rand.IntN(100),
+		Id:     uuid.New(),
+	}
+
+	addListing(work)
+	return work
 }
 
 // TODO:
-func getSeries(id string) *Series {
-	return GetMockSeries(100)
+func getWork(id uuid.UUID) *Work {
+	return GetExistingMockDb().GetWork(id)
 }
+
+// TODO:
+func getSeries(id uuid.UUID) *Series {
+	return GetExistingMockDb().GetSeries(id)
+}
+
+// TODO:
+func getContent(id uuid.UUID) Content {
+	return GetExistingMockDb().Contents[id]
+}
+
+// TODO:
+func GetImage(id uuid.UUID) *Image {
+	return GetExistingMockDb().GetImage(id)
+}
+
+func getUuidFromId(id string) uuid.UUID {
+	retId, err := uuid.Parse(id)
+	if err != nil {
+		panic(err)
+	}
+
+	return retId
+}
+
+// TODO: improve matching mime -> ContentType
+func getFileContentType(file multipart.File) (ContentType, bool) {
+	buf := make([]byte, 512)
+	n, _ := file.Read(buf)
+	mimeType := http.DetectContentType(buf[:n])
+	file.Seek(0, io.SeekStart)
+	ct, ok := MimeToContentType[mimeType]
+
+	if !ok {
+		log.Println("mime type " + mimeType + " not found in mime map")
+	}
+
+	return ct, ok
+}
+
+func storeUploadedFormFile(r *http.Request) (Content, error) {
+	file, header, err := r.FormFile("upload")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	} else {
+		ct, validCt := getFileContentType(file)
+		if !validCt || ct == TextType {
+			return nil, errors.New("invalid file type uploaded")
+		}
+
+		defer file.Close()
+
+		localPath := ct.toLocalPath(header.Filename)
+
+		dst, err := os.Create(localPath)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		} else {
+			defer dst.Close()
+			io.Copy(dst, file)
+		}
+
+		return ct.createNew([]string{ct.toSourceUrl(header.Filename)}), nil
+	}
+}
+
+func handleContentUpload(r *http.Request, work *Work) error {
+	content, err := storeUploadedFormFile(r)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	work.addContent(content)
+
+	return nil
+}
+
+// TODO:
+func getAllListings() iter.Seq[Listing] {
+	return maps.Values(GetExistingMockDb().Listings)
+}
+
+// Assume Contents is always sorted by index
+func moveContentUp(w *Work, idx int) {
+	if idx == 0 {
+		return
+	}
+
+	// TODO: remove
+	for i, c := range w.Contents {
+		if c.GetIndex() != i {
+			panic("contents slice is not sorted by index")
+		}
+	}
+
+	w.Contents[idx].SetIndex(idx - 1)
+	w.Contents[idx-1].SetIndex(idx)
+	w.Contents[idx], w.Contents[idx-1] = w.Contents[idx-1], w.Contents[idx]
+}
+
+// Assume Contents is always sorted by index
+func moveContentDown(w *Work, idx int) {
+	if idx >= len(w.Contents)-1 {
+		return
+	}
+
+	// TODO: remove
+	for i, c := range w.Contents {
+		if c.GetIndex() != i {
+			panic("contents slice is not sorted by index")
+		}
+	}
+
+	w.Contents[idx].SetIndex(idx + 1)
+	w.Contents[idx+1].SetIndex(idx)
+	w.Contents[idx], w.Contents[idx+1] = w.Contents[idx+1], w.Contents[idx]
+}
+
+// Assume Contents is always sorted by index with no gaps
+// TODO:
+func deleteContent(w *Work, contentId uuid.UUID) {
+	idx := getContent(contentId).GetIndex()
+	cs := w.Contents
+
+	w.Contents = append(cs[:idx], cs[idx+1:]...)
+	for i := idx; i < len(w.Contents); i++ {
+		w.Contents[i].SetIndex(i)
+	}
+
+	delete(GetExistingMockDb().Contents, contentId)
+}
+
+// ----------------------------
+//
+//	HANDLERS HANDLERS HANDLERS
+//
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	templ := template.Must(template.New("home.html").Funcs(template.FuncMap{
 		"formatTime": formatTimeRequired,
 	}).ParseFiles("./resources/home.html"))
 
-	listings := getNWorkListings(v2.IntN(10))
+	listings := getAllListings()
+
 	templ.Execute(w, listings)
 }
 
-func workHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
+func viewWorkHandler(w http.ResponseWriter, r *http.Request) {
+	id := getUuidFromId(r.PathValue("id"))
 	work := getWork(id)
-
 	templ := template.Must(template.New("work.html").Funcs(template.FuncMap{
 		"isText":             func(ct ContentType) bool { return ct == TextType },
 		"isSound":            func(ct ContentType) bool { return ct == SoundType },
@@ -283,8 +669,8 @@ func workHandler(w http.ResponseWriter, r *http.Request) {
 	templ.Execute(w, work)
 }
 
-func seriesHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
+func viewSeriesHandler(w http.ResponseWriter, r *http.Request) {
+	id := getUuidFromId(r.PathValue("id"))
 	series := getSeries(id)
 
 	templ := template.Must(template.New("series.html").Funcs(template.FuncMap{
@@ -294,21 +680,120 @@ func seriesHandler(w http.ResponseWriter, r *http.Request) {
 	templ.Execute(w, series)
 }
 
+func createWorkHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		createWorkGetHandler(w, r)
+	case http.MethodPost:
+		createWorkPostHandler(w, r)
+	default:
+		return
+	}
+}
+
+func createWorkGetHandler(w http.ResponseWriter, r *http.Request) {
+	newWork := createNewWork()
+	addListing(newWork)
+
+	templ := template.Must(template.ParseFiles("./resources/canvas.html"))
+	templ.Execute(w, newWork)
+}
+
+func createWorkPostHandler(w http.ResponseWriter, r *http.Request) {
+
+	action := r.FormValue("action")
+	id := getUuidFromId(r.FormValue("work-id"))
+	work := getWork(id)
+
+	switch action {
+	case "move-up":
+		idx, err := strconv.Atoi(r.FormValue("content-index"))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Unexpected error moving content.", http.StatusInternalServerError)
+			return
+		}
+
+		moveContentUp(work, idx)
+
+	case "move-down":
+		idx, err := strconv.Atoi(r.FormValue("content-index"))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Unexpected error moving content.", http.StatusInternalServerError)
+			return
+		}
+
+		moveContentDown(work, idx)
+
+	case "delete-content":
+		contentId := getUuidFromId(r.FormValue("content-id"))
+		deleteContent(work, contentId)
+
+	case "upload":
+		err := r.ParseMultipartForm(10 << 20) // TODO: increase?
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Content uploaded is too large.", http.StatusBadRequest)
+			return
+		}
+
+		err = handleContentUpload(r, work)
+		if err != nil {
+			http.Error(w, "Error during content upload.", http.StatusInternalServerError)
+			return
+		}
+
+	default:
+	}
+
+	templ := template.Must(template.ParseFiles("./resources/canvas.html"))
+	templ.Execute(w, work)
+}
+
+func editWorkHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		editWorkGetHandler(w, r)
+	case http.MethodPost:
+		createWorkPostHandler(w, r)
+	default:
+		return
+	}
+}
+
+func editWorkGetHandler(w http.ResponseWriter, r *http.Request) {
+	id := getUuidFromId(r.FormValue("id"))
+	work := getWork(id)
+	templ := template.Must(template.ParseFiles("./resources/canvas.html"))
+	templ.Execute(w, work)
+}
+
+// TODO:
+func organizeWorksHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("hi")
+}
+
 func main() {
 	for _, contentType := range AllContentTypes {
 		if contentType == TextType {
 			continue
 		}
 
-		mediaDir := contentType.mediaDir()
-		urlPrefix := contentType.urlPrefix()
+		mediaDir := "." + contentType.mediaDir() // ./{resourcefolder}/{media}/{contentType (plural)}/
+		urlPrefix := contentType.urlPrefix()     // /{contentType (singular)}/{with}/{name}/{fileName}/
 
-		fs := http.FileServer(http.Dir(mediaDir))
-		http.Handle(urlPrefix, http.StripPrefix(urlPrefix, fs))
+		contentTypeFileServer := http.FileServer(http.Dir(mediaDir))
+		http.Handle(urlPrefix, http.StripPrefix(urlPrefix, contentTypeFileServer))
 	}
 
-	http.HandleFunc("/works/with/id/{id}", workHandler)
-	http.HandleFunc("/series/with/id/{id}", seriesHandler)
+	http.HandleFunc("/work/number/{id}", viewWorkHandler)
+	http.HandleFunc("/series/number/{id}", viewSeriesHandler)
+
+	http.HandleFunc("/compose/work", createWorkHandler)
+	http.HandleFunc("/compose/work/number/{id}", editWorkHandler)
+
+	http.HandleFunc("/organize/works/by/{name}", organizeWorksHandler)
 	http.HandleFunc("/{$}", homeHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- mocks.go
+++ mocks.go
@@ -3,13 +3,149 @@ package main
 import (
 	"fmt"
 	"log"
+
+	"github.com/google/uuid"
 )
+
+func init() {
+	log.Println("calling MOCK init()")
+
+	for _, l := range GetNMockListings(10) {
+		mockExistingDb.AddListing(l)
+	}
+
+	for _, c := range GetNMockContents(20) {
+		mockExistingDb.AddContent(c)
+	}
+}
+
+type Database interface {
+	AddContent(c Content)
+	AddListing(l Listing)
+
+	GetWork(id uuid.UUID) *Work
+	GetSeries(id uuid.UUID) *Series
+	GetText(id uuid.UUID) *Text
+	GetImage(id uuid.UUID) *Image
+	GetSound(id uuid.UUID) *Sound
+	GetVideo(id uuid.UUID) *Video
+}
+
+type WorksKeeperDB struct {
+	Listings map[uuid.UUID]Listing
+	Contents map[uuid.UUID]Content
+}
+
+var mockExistingDb = &WorksKeeperDB{
+	Listings: make(map[uuid.UUID]Listing),
+	Contents: make(map[uuid.UUID]Content),
+}
+
+func (db *WorksKeeperDB) AddContent(c Content) {
+	db.Contents[c.GetId()] = c
+}
+
+func (db *WorksKeeperDB) AddListing(l Listing) {
+	db.Listings[l.GetId()] = l
+
+	switch x := l.(type) {
+
+	case *Work:
+		for _, c := range x.Contents {
+			db.AddContent(c)
+		}
+
+	case *Series:
+		for _, item := range x.Works {
+			db.AddListing(item)
+		}
+	}
+}
+
+func (db *WorksKeeperDB) GetWork(id uuid.UUID) *Work {
+	v, ok := db.Listings[id]
+	if !ok {
+		panic("GetWork: no listing with that ID")
+	}
+	w, ok := v.(*Work)
+	if !ok {
+		panic(fmt.Sprintf("GetWork: listing %T is not a Work", v))
+	}
+	return w
+}
+
+func (db *WorksKeeperDB) GetSeries(id uuid.UUID) *Series {
+	v, ok := db.Listings[id]
+	if !ok {
+		panic("GetSeries: no listing with that ID")
+	}
+	s, ok := v.(*Series)
+	if !ok {
+		panic(fmt.Sprintf("GetSeries: listing %T is not a Series", v))
+	}
+	return s
+}
+
+func (db *WorksKeeperDB) GetText(id uuid.UUID) *Text {
+	v, ok := db.Contents[id]
+	if !ok {
+		panic("GetText: no content with that ID")
+	}
+	t, ok := v.(*Text)
+	if !ok {
+		panic(fmt.Sprintf("GetText: content %T is not a Text", v))
+	}
+	return t
+}
+
+func (db *WorksKeeperDB) GetImage(id uuid.UUID) *Image {
+	v, ok := db.Contents[id]
+	if !ok {
+		panic("GetImage: no content with that ID")
+	}
+	img, ok := v.(*Image)
+	if !ok {
+		panic(fmt.Sprintf("GetImage: content %T is not an Image", v))
+	}
+	return img
+}
+
+func (db *WorksKeeperDB) GetSound(id uuid.UUID) *Sound {
+	v, ok := db.Contents[id]
+	if !ok {
+		panic("GetSound: no content with that ID")
+	}
+	s, ok := v.(*Sound)
+	if !ok {
+		panic(fmt.Sprintf("GetSound: content %T is not a Sound", v))
+	}
+	return s
+}
+
+func (db *WorksKeeperDB) GetVideo(id uuid.UUID) *Video {
+	v, ok := db.Contents[id]
+	if !ok {
+		panic("GetVideo: no content with that ID")
+	}
+	video, ok := v.(*Video)
+	if !ok {
+		panic(fmt.Sprintf("GetVideo: content %T is not a Video", v))
+	}
+	return video
+}
+
+func GetExistingMockDb() *WorksKeeperDB {
+	log.Println("calling MOCK GetExistingMockDb()")
+
+	return mockExistingDb
+}
 
 func GetMockText() *Text {
 	log.Println("calling MOCK GetMockText()")
 
 	return &Text{
 		Text: "Mock text!",
+		Id:   uuid.New(),
 	}
 }
 
@@ -17,7 +153,9 @@ func GetMockSound() *Sound {
 	log.Println("calling MOCK GetMockSound()")
 
 	return &Sound{
-		SoundPaths: []string{"/sounds/with/name/609562_migfus20_background-music.ogg"},
+		SoundPaths: []string{getBaseUrl() + SoundType.urlPrefix() + "609562_migfus20_background-music.ogg"},
+		Caption:    "Sound caption",
+		Id:         uuid.New(),
 	}
 }
 
@@ -25,7 +163,9 @@ func GetMockVideo() *Video {
 	log.Println("calling MOCK GetMockVideo()")
 
 	return &Video{
-		VideoPaths: []string{"/videos/with/name/14044733_1080_1920_48fps(2).mp4"},
+		VideoPaths: []string{getBaseUrl() + VideoType.urlPrefix() + "14044733_1080_1920_48fps(2).mp4"},
+		Caption:    "Testing a video caption",
+		Id:         uuid.New(),
 	}
 }
 
@@ -33,7 +173,9 @@ func GetMockImage() *Image {
 	log.Println("calling MOCK GetMockImage()")
 
 	return &Image{
-		ImagePaths: []string{"/images/with/name/IMG20250819173509~2.jpg"},
+		ImagePaths: []string{getBaseUrl() + ImageType.urlPrefix() + "IMG20250819173509~2.jpg"},
+		Caption:    "An image caption",
+		Id:         uuid.New(),
 	}
 }
 
@@ -55,6 +197,18 @@ func GetMockWork(i int) *Work {
 			GetMockVideo(),
 			GetMockImage(),
 		},
+		Id: uuid.New(),
+	}
+}
+
+func GetMockEmptyWork() *Work {
+	log.Println("calling MOCK GetMockEmptyWork()")
+
+	return &Work{
+		Title:    "Untitled",
+		Length:   0,
+		Contents: []Content{},
+		Id:       uuid.New(),
 	}
 }
 
@@ -66,6 +220,7 @@ func GetNonNestedMockSeries(i int) *Series {
 		Works: []Listing{
 			GetMockWork(30),
 		},
+		Id: uuid.New(),
 	}
 }
 
@@ -79,26 +234,47 @@ func GetMockSeries(i int) *Series {
 			GetMockWork(2),
 			GetNonNestedMockSeries(29),
 		},
+		Id: uuid.New(),
 	}
 }
 
 func GetNMockListings(n int) []Listing {
-	log.Println("calling MOCK GetMockListings()")
+	log.Println("calling MOCK GetNMockListings()")
 
 	list := make([]Listing, 0, n)
 
 	for j := range n {
-		i := j * 10
+		i := j + 1
 		if i%3 == 0 {
-			list = append(list, GetMockSeries(i+1))
+			list = append(list, GetMockSeries(i))
 		} else {
-			list = append(list, GetMockWork(i+1))
+			list = append(list, GetMockWork(i))
 		}
 	}
 
 	return list
 }
 
+func GetNMockContents(n int) []Content {
+	log.Println("calling MOCK GetNMockContents()")
+
+	contents := make([]Content, 0, n)
+
+	for i := range n {
+		switch i % 4 {
+		case 0:
+			contents = append(contents, GetMockText())
+		case 1:
+			contents = append(contents, GetMockSound())
+		case 2:
+			contents = append(contents, GetMockImage())
+		case 3:
+			contents = append(contents, GetMockVideo())
+		}
+	}
+
+	return contents
+}
 func GetMockBaseUrl() string {
 	log.Println("calling MOCK GetMockBaseUrl()")
 
